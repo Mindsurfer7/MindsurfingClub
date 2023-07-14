26c226dbf26125297e34bdd2916a42774a273ccb
Mindsurfer7 <bamboocymba@icloud.com>
2023-07-15 03:21:38 +0600

    x

--- src/pages/about/UI/AboutPage.tsx
+++ src/pages/about/UI/AboutPage.tsx
@@ -2,7 +2,6 @@ import { useTranslation } from "react-i18next";
 
 const AboutPage = () => {
   const { t } = useTranslation("about");
-  console.log("about page");
 
   return <div>{t("description")}</div>;
 };
