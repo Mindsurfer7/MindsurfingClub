9587811dea8e31413f954dbc22c6cabf5b46f27c
Mindsurfer7 <bamboocymba@icloud.com>
2023-07-14 22:42:56 +0600

    404 uppercase

--- src/pages/404/Error404.tsx
+++ src/pages/404/Error404.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { classNames } from "shared/lib/classNames/classNames";
-import cls from "./Error404.module.scss";
+import cls from "./error404.module.scss";
 
 interface Error404Props {
   className?: string;
